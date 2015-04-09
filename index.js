98845f2be854c9d801578e10d1cd104e5a151321
Christoher Carson <chris@nowzoo.com>
2015-04-09 13:37:35 -0500

    added inquirer

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = function(){
-    console.log('hi');
-};
+var inquirer = require("inquirer");
+var questions = [
+    {
+        type: 'input',
+        name: 'site_title',
+        message: 'Site Title'
+    }
+];
+inquirer.prompt(questions, function( answers ) {
+    console.log(answers);
+    console.log(process.cwd());
+});
+
+
