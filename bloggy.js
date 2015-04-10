7f2cb173a17247ff29c93cb48cfd4ac97b23ac96
Christoher Carson <chris@nowzoo.com>
2015-04-10 14:13:21 -0500

    added executable

--- bloggy.js
+++ bloggy.js
@@ -1,10 +1,10 @@
 var program = require('commander');
-
+var cwd = process.cwd();
 program
     .command('init')
     .description('initialize a site')
     .action(function(){
-        console.log('Initializing a site in %s', process.cwd());
+        console.log('Initializing a site in %s', cwd);
     });
 program.parse(process.argv);
 
