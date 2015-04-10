65b76c958eac0e6c080d609b5b2ce4e662d26041
Christoher Carson <chris@nowzoo.com>
2015-04-10 14:02:19 -0500

    package update

--- bloggy.js
+++ bloggy.js
@@ -4,7 +4,7 @@ program
     .command('init')
     .description('initialize a site')
     .action(function(){
-        console.log('initializing a site in %s', process.cwd());
+        console.log('Initializing a site in %s', process.cwd());
     });
 program.parse(process.argv);
 
